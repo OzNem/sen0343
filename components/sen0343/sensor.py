839d62a4cb3c06cdd2954cd84a2e32f12fa00b16
OzNem <34363789+OzNem@users.noreply.github.com>
2026-01-23 22:57:14 +1100

    Update sensor.py

--- components/sen0343/sensor.py
+++ components/sen0343/sensor.py
@@ -1,5 +1,3 @@
-# sen0343/sensor.py 
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
@@ -7,9 +5,7 @@ from esphome.components import sensor, i2c
 from esphome.const import (
     CONF_ID,
     CONF_ADDRESS,
-    CONF_UPDATE_INTERVAL,
     UNIT_PASCAL,
-    DEVICE_CLASS_PRESSURE,
 )
 from esphome import automation
 
@@ -20,11 +16,14 @@ from . import sen0343_ns, SEN0343
 
 CONF_SCALE_FACTOR = 'scale_factor'
 
-PLATFORM_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+# Use sensor.PLATFORM_SCHEMA so standard sensor options (name, id, filters, update_interval, etc.)
+# are accepted, and we can extend with our custom options.
+PLATFORM_SCHEMA = sensor.PLATFORM_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(SEN0343),
-    cv.Optional(CONF_ADDRESS, default=0x01): cv.hex_uint8_t,  # default register/address can be overridden in YAML
+    cv.Optional(CONF_ADDRESS, default=0x01): cv.hex_uint8_t,
     cv.Optional(CONF_SCALE_FACTOR, default=64.0): cv.float_,
 }).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], SEN0343)
