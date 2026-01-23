86f2931ab8908ca776af7badcc37e2ffb4c7401e
OzNem <34363789+OzNem@users.noreply.github.com>
2026-01-23 22:08:33 +1100

    Update __init__.py

--- components/sen0343/__init__.py
+++ components/sen0343/__init__.py
@@ -1,5 +1,7 @@
-# must be here 
 import esphome.codegen as cg
 from esphome.components import sensor
+from esphome.const import CONF_ID
+
 sen0343_ns = cg.esphome_ns.namespace('sen0343')
+# C++ class name SEN0343, inherits Component and sensor::Sensor
 SEN0343 = sen0343_ns.class_('SEN0343', cg.Component, sensor.Sensor)
