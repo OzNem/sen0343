291eb86b8fbffbce6cbe4ae89872178afcaae0e6
OzNem <34363789+OzNem@users.noreply.github.com>
2026-01-23 20:41:25 +1100

    Update __init__.py
    AI say to add this. Yesterday it said to leave it blank. Lets see what happens now

--- components/sen0343/__init__.py
+++ components/sen0343/__init__.py
@@ -1,1 +1,5 @@
 # must be here 
+import esphome.codegen as cg
+from esphome.components import sensor
+sen0343_ns = cg.esphome_ns.namespace('sen0343')
+SEN0343 = sen0343_ns.class_('SEN0343', cg.Component, sensor.Sensor)
